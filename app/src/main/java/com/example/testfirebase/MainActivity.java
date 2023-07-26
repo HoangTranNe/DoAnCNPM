897354c1776e814271721f2cda1c73c2f607b1db
hoangtran <tranbaohoang70@gmail.com>
2023-07-27 02:06:27 +0700

    thêm đăng nhập bằng google

---
+++ app/src/main/java/com/example/testfirebase/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.testfirebase;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    FirebaseFirestore firestore;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        firestore = FirebaseFirestore.getInstance();
+        Map<String, Object> users = new HashMap<>();
+        users.put("fistname","hoang");
+        users.put("lastname","hoang");
+        users.put("desciption","hoang");
+
+
+        firestore.collection("users").add(users).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+            @Override
+            public void onSuccess(DocumentReference documentReference) {
+                Toast.makeText(getApplicationContext(),"Success", Toast.LENGTH_LONG).show();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(getApplicationContext(),"Failures",Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+
+
+}
---
+++ app/src/main/java/com/example/testfirebase/login2Activity.java
@@ -0,0 +1,15 @@
+package com.example.testfirebase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+public class login2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login2);
+    }
+}
